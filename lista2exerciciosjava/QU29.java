1ba8d47977d8d0316c75ca5ce6e60bd3745eaad9
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 16:19:21 -0300

    Update QU29.java

--- lista2exerciciosjava/QU29.java
+++ lista2exerciciosjava/QU29.java
@@ -1,1 +1,16 @@
+import java.util.Arrays;
+
+public class QU29 {
+    public static void main(String[] args) {
+        int N = 10;
+        int[] lucas = new int[N];
+        lucas[0] = 2;
+        lucas[1] = 1;
+
+        for (int i = 2; i < N; i++) {
+            lucas[i] = lucas[i-1] + lucas[i-2];
+        }
+        System.out.println(Arrays.toString(lucas));
+    }
+}
 
