24d02429d38e4cc265dcbab19a6540d54a471980
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 18:29:53 -0300

    Update QU33.java

--- lista2exerciciosjava/QU33.java
+++ lista2exerciciosjava/QU33.java
@@ -1,1 +1,14 @@
+import java.util.Arrays;
+
+public class QU33 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30};
+        int novo = 40;
+
+        int[] novoArray = Arrays.copyOf(array, array.length + 1);
+        novoArray[novoArray.length - 1] = novo;
+
+        System.out.println(Arrays.toString(novoArray));
+    }
+}
 
