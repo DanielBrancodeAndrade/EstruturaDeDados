1519698ca098fab6dcfe69b3da51f64bc3204904
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 11:34:19 -0300

    Update QU23.java

--- lista2exerciciosjava/QU23.java
+++ lista2exerciciosjava/QU23.java
@@ -1,1 +1,15 @@
+import java.util.Arrays;
+
+public class QU23 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        int[] b = {4, 5, 6};
+        int[] c = new int[a.length + b.length];
+
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+
+        System.out.println(Arrays.toString(c));
+    }
+}
 
