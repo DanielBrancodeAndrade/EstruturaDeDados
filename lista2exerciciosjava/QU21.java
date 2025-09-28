31caaa00bcfb0826c102c981d76f7901e5b886b3
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 11:01:58 -0300

    Update QU21.java

--- lista2exerciciosjava/QU21.java
+++ lista2exerciciosjava/QU21.java
@@ -1,1 +1,13 @@
-dsadsads
+public class QU21 {
+    public static void main(String[] args) {
+        char[] letras = {'a', 'b', 'e', 'x', 'i'};
+
+        for (int i = 0; i < letras.length; i++) {
+            if ("aeiouAEIOU".indexOf(letras[i]) != -1) {
+                letras[i] = '*';
+            }
+        }
+
+        System.out.println(letras);
+    }
+}
