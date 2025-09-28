07bec03992516aa1b020acd52e6d7bc06bb8e1e2
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 11:01:25 -0300

    Update QU22.java

--- lista2exerciciosjava/QU22.java
+++ lista2exerciciosjava/QU22.java
@@ -1,1 +1,13 @@
+public class QU22 {
+    public static void main(String[] args) {
+        char[] letras = {'a', 'b', 'e', 'x', 'i'};
+        int cont = 0;
+
+        for (char c : letras) {
+            if ("aeiouAEIOU".indexOf(c) != -1) cont++;
+        }
+
+        System.out.println("Número de vogais: " + cont);
+    }
+}
 
