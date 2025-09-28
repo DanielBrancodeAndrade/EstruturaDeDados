ad23a087279f98eaf905fe34c6dcfb4761a69ebf
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 16:49:15 -0300

    Update QU30.java

--- lista2exerciciosjava/QU30.java
+++ lista2exerciciosjava/QU30.java
@@ -1,1 +1,13 @@
+import java.util.*;
+
+public class QU30 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 3, 4, 4, 5};
+        Set<Integer> set = new LinkedHashSet<>();
+
+        for (int n : array) set.add(n);
+
+        System.out.println(set);
+    }
+}
 
