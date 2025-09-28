3aa5ec5c9ca3fc8072d86211befa8753d399f6f9
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 17:36:25 -0300

    Update QU32.java

--- lista2exerciciosjava/QU32.java
+++ lista2exerciciosjava/QU32.java
@@ -1,1 +1,12 @@
+import java.util.Arrays;
+
+public class QU32 {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 9};
+        int chave = 7;
+
+        int pos = Arrays.binarySearch(array, chave);
+        System.out.println("Posição: " + pos);
+    }
+}
 
