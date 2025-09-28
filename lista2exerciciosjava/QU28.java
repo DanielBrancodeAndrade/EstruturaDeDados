12bdbd01f92b5656e681564f38d97bbfcf18d0a4
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 12:44:46 -0300

    Update QU28.java

--- lista2exerciciosjava/QU28.java
+++ lista2exerciciosjava/QU28.java
@@ -1,1 +1,16 @@
+import java.util.Arrays;
+
+public class QU28 {
+    public static void main(String[] args) {
+        int N = 10;
+        int[] fib = new int[N];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i < N; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        System.out.println(Arrays.toString(fib));
+    }
+}
 
