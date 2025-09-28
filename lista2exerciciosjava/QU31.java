0f489971d125f4cd1f39da323922950605251c6c
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 17:20:41 -0300

    Update QU31.java

--- lista2exerciciosjava/QU31.java
+++ lista2exerciciosjava/QU31.java
@@ -1,1 +1,16 @@
+import java.util.Arrays;
+
+public class QU31 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int primeiro = array[0];
+
+        for (int i = 0; i < array.length - 1; i++) {
+            array[i] = array[i+1];
+        }
+        array[array.length - 1] = primeiro;
+
+        System.out.println(Arrays.toString(array));
+    }
+}
 
