f65273181784ed2ce95636c898fa16ea6287fb11
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 11:41:25 -0300

    Update QU24.java

--- lista2exerciciosjava/QU24.java
+++ lista2exerciciosjava/QU24.java
@@ -1,1 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class QU24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] array = {10, 20, 30, 40, 50};
+
+        System.out.print("Digite a posição: ");
+        int pos = sc.nextInt();
+        System.out.print("Digite o novo elemento: ");
+        int elem = sc.nextInt();
+
+        int[] novoArray = new int[array.length + 1];
+        for (int i = 0, j = 0; i < novoArray.length; i++) {
+            if (i == pos) {
+                novoArray[i] = elem;
+            } else {
+                novoArray[i] = array[j++];
+            }
+        }
+        System.out.println(Arrays.toString(novoArray));
+    }
+}
 
