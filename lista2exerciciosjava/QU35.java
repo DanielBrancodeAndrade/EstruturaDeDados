59313219b136a9d2506c87f916caa215a9bc02d6
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 19:09:42 -0300

    Update QU35.java

--- lista2exerciciosjava/QU35.java
+++ lista2exerciciosjava/QU35.java
@@ -1,1 +1,17 @@
+import java.util.Arrays;
+
+public class QU35 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40, 50};
+        int pos = 2; 
+
+        int[] novoArray = new int[array.length - 1];
+        for (int i = 0, j = 0; i < array.length; i++) {
+            if (i != pos) {
+                novoArray[j++] = array[i];
+            }
+        }
+        System.out.println(Arrays.toString(novoArray));
+    }
+}
 
