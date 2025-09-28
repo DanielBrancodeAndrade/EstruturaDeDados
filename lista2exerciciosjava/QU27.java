ef362e844a112f5437433cd11512d3414126c298
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 12:21:13 -0300

    Update QU27.java

--- lista2exerciciosjava/QU27.java
+++ lista2exerciciosjava/QU27.java
@@ -1,1 +1,24 @@
+import java.util.*;
+
+public class QU27 {
+    public static void main(String[] args) {
+        int N = 1000;
+        List<Integer> perfeitos = new ArrayList<>();
+
+        for (int i = 2; i < N; i++) {
+            if (ehPerfeito(i)) {
+                perfeitos.add(i);
+            }
+        }
+        System.out.println(perfeitos);
+    }
+
+    public static boolean ehPerfeito(int n) {
+        int soma = 1;
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) soma += i;
+        }
+        return soma == n;
+    }
+}
 
