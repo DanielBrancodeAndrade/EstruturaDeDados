b4448f68911ceae8aec4d6c669b84574c7871de3
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 12:12:27 -0300

    Update QU25.java

--- lista2exerciciosjava/QU25.java
+++ lista2exerciciosjava/QU25.java
@@ -1,1 +1,15 @@
+public class QU25 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 2, 1};
+        boolean palindromo = true;
+
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                palindromo = false;
+                break;
+            }
+        }
+        System.out.println("É palíndromo? " + palindromo);
+    }
+}
 
