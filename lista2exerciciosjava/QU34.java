7d5ffcab257c4fce11f61990aa01c068ab2961cb
DanielBrancodeAndrade <202421190035@ifba.edu.br>
2025-09-28 18:50:44 -0300

    Update QU34.java

--- lista2exerciciosjava/QU34.java
+++ lista2exerciciosjava/QU34.java
@@ -1,1 +1,20 @@
+import java.util.Arrays;
+
+public class QU34 {
+    public static void main(String[] args) {
+        int[] array = {10, 20, 30, 40};
+        int pos = 2;
+        int elem = 99;
+
+        int[] novoArray = new int[array.length + 1];
+        for (int i = 0, j = 0; i < novoArray.length; i++) {
+            if (i == pos) {
+                novoArray[i] = elem;
+            } else {
+                novoArray[i] = array[j++];
+            }
+        }
+        System.out.println(Arrays.toString(novoArray));
+    }
+}
 
